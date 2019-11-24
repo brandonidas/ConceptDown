b367cef54b3094a1bdc93fdc08e9a6f365851eeb
Brandon Tong Jia Xin <brandonidas@gmail.com>
2019-11-24 18:39:03 +0800

    Courses-Topics-Concepts Dashboard Skeleton Logic

--- CDClient/Concept.cs
+++ CDClient/Concept.cs
@@ -1,10 +1,10 @@
 namespace CDClient
 {
-    public class Concept : Knowde
+    public class Concept // : Knowde
     {
-        // public string Title { get; set; }
-        // public int Priority { get; set; }
-        // public int Mastery { get; set; }
+        public string Name { get; set; }
+        public int Priority { get; set; }
+        public int Mastery { get; set; }
         public string Remarks { get; set; }
 
     }
